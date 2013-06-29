6bd8791c2b9cdd74c8540dfdf13e220c98ee089d
Eric Gonzalez <ericxgonzalez@gmail.com>
2013-06-28 19:53:41 -0500

    Uploaded script

---
+++ primecheck.js
@@ -0,0 +1,12 @@
+#! /usr/bin/env node
+
+function PrimeCheck (n) {
+    if (n < 2) return false;
+    var ch = Math.sqrt(n);
+    for (var i = 2; i <= ch; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
